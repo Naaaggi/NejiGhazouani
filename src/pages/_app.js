9a3921b59c1831acaf434953cddbc1e2e45ee287
Naaaggi <naguigh23@gmail.com>
2022-01-14 21:49:43 +0100

    changed _app.js back

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,13 @@
-import Head from "next/head";
-import Navigation from "../components/Navigation";
-import "../styles/globals.css";
+import Theme from '../styles/theme';
+import Head from "next/head"
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <div>
-      <Head>
-      <title>Neji Ghazouani - Full Stack Developer</title>
-        <meta charset="UTF-8" />
-        <meta name="description" content="Neji Ghazouani - Full Stack Developer" />
-      </Head>
-      <Navigation />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Theme>
+        <Component {...pageProps} />
+      </Theme>
+    </>
   );
 }
-
-export default MyApp;
+ 
